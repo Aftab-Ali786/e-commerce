ffeaf2fe3c3059ebbbea49e4565d3fa571fcd4b6
Aftab ali <aftabalirizvi123@gmail.com>
2025-02-04 01:44:28 +0530

    update cart-icon for the image torender

--- src/component/custom-button/custom-button.component.jsx
+++ src/component/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React, { Children } from "react";
 
 import './custom-button.styles.scss'
 
-const CustomButton =({children, ...otherProps})=>(
-    <button className="custom-button" {...otherProps}>{children}</button>
+const CustomButton =({children,isGoogleSignIn, inverted, ...otherProps})=>(
+    <button className={`${inverted ? 'inverted':''}${isGoogleSignIn ?'isGoogleSignIn':''}custom-button`}{...otherProps}>{children}</button>
 );
 
 export default CustomButton;
